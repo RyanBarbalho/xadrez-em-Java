738acc40ba255ce79648a9ea4a3b467c3382c10d
Ryan Barbalho <rgtbc@ic.ufal.br>
2023-06-13 15:13:16 -0300

    Chess Exception e ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,12 +25,18 @@ public class ChessMatch {
                 matriz[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
+        // retorna a matriz de pecas do xadrez
         return matriz;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        // coluna e linha do xadrez
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
