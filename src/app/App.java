d53daf8171c8706565d5a0b3bed642035680499f
Ryan Barbalho <rgtbc@ic.ufal.br>
2023-06-13 11:34:49 -0300

    classe posision added

---
+++ src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+import tabuleiro.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
+    }
+}
