a9d49c7738b2f534f8f7e094a25ad43b808b8b52
Ryan Barbalho <rgtbc@ic.ufal.br>
2023-06-14 16:08:11 -0300

    Queen adicionado

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,101 @@
+package chess.pieces;
+
+import chess.ChessPiece;
+import chess.Color;
+import tabuleiro.Board;
+import tabuleiro.Position;
+
+public class Queen extends ChessPiece {
+
+    public Queen(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Q";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        // matriz de movimentos possiveis, todos nulos.
+        boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        // posicao da peca atual
+        Position p = new Position(0, 0);
+        // a cima
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // a esquerda
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // a direita
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // a baixo
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // noroeste
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // nordeste
+        p.setValues(position.getRow() + -1, position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // sudeste
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        // sudoeste
+        p.setValues(position.getRow() + 1, position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+
+        return matriz;
+    }
+
+}
